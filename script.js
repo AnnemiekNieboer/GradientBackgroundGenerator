41dc23088c79f09e59b90d5af9411357653b2b0d
AnnemiekNieboer <67008458+AnnemiekNieboer@users.noreply.github.com>
2020-07-15 14:38:56 +0200

    aanpassing op css.textContent en css update
    De teksttoevoeging op H3 deed het eerst niet bij getNewGradient, omdat deze na de return stond. Return verwijdert (was niet nodig) en functie gemaakt van de tekstoevoeging
    Daarnaast een kleine css update gedaan

--- script.js
+++ script.js
@@ -4,13 +4,15 @@ var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 var randomColorButton = document.getElementById("randomColor");
 
+
+
 function setGradient() {
 	body.style.background = "linear-gradient(to right, " 
 	+ color1.value 
 	+ ", " 
 	+ color2.value 
 	+ ")";
-	css.textContent = body.style.background + ";";	
+	getGradientText();	
 }
 
 color1.addEventListener("input", setGradient);
@@ -22,11 +24,15 @@ function getNewGradient() {
 	var c2 = '#'+Math.random().toString(16).substr(-6);
 	color1.value = c1;
 	color2.value = c2;
-	return body.style.background = "linear-gradient(to right, " 
+	body.style.background = "linear-gradient(to right, " 
 	+ c1
 	+ ", " 
 	+ c2
 	+ ")";
+	getGradientText();
+}
+
+function getGradientText() {
 	css.textContent = body.style.background + ";";
 }
 
